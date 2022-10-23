1ba33a1d8a56fdfb40c5ed5c85fa8dbfce7aa322
Joshua Valdez <josh@augnos.com>
2022-10-23 16:42:03 -0700

    Added meta previews and favicon

--- flask_app/controllers/controllers.py
+++ flask_app/controllers/controllers.py
@@ -1,7 +1,13 @@
-from flask import render_template
+from flask import render_template, send_from_directory
+import os
 from flask_app import app
 
 
 @app.route('/')
 def home():
     return render_template("index.html")
+
+# @app.route('/favicon.ico')
+# def favicon():
+#     return send_from_directory(os.path.join(app.root_path, 'static'),
+#         'favicon.ico',mimetype='image/vnd.microsoft.icon')
